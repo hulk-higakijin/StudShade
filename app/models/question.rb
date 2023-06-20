94ef072015172453fc125aa2b8745c78984c00a4
hulk-higakijin <hm385.chejptks@gmail.com>
2023-06-20 14:21:11 +0900

    fix: QuestionsController

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,6 @@
 class Question < ApplicationRecord
   belongs_to :user
+
+  validates :title, presence: true, length: { in: 5..30 }
+  validates :content, presence: true, length: { in: 10..1000 }
 end
