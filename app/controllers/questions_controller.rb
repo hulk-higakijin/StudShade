10415c92b67a92b2fbdc56a04e44dcb8034311f6
hulk-higakijin <hm385.chejptks@gmail.com>
2023-06-21 11:44:50 +0900

    style: questions#new, #edit

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,7 +3,7 @@ class QuestionsController < ApplicationController
   before_action :authenticate_user!, only: %i[new create edit update destroy]
 
   def index
-    @questions = Question.all
+    @questions = Question.all.order(created_at: :desc)
   end
 
   def show; end
