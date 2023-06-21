3621655f975b4ce428a44b363090ecd49a6d4ddc
hulk-higakijin <hm385.chejptks@gmail.com>
2023-06-21 12:20:42 +0900

    feat: add name to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ApplicationRecord
 
   has_many :questions, dependent: :destroy
   has_many :answers, dependent: :destroy
+
+  validates :name, presence: true, length: { in: 2..20 }
 end
