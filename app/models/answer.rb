5ef5596cfaba8c67fba0e6005b32c41de53ce91a
hulk-higakijin <hm385.chejptks@gmail.com>
2023-06-23 23:27:43 +0900

    feat: add answer's validation between user & question

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,4 +3,5 @@ class Answer < ApplicationRecord
   belongs_to :question
 
   validates :content, presence: true, length: { in: 10..200 }
+  validates :user_id, uniqueness: { scope: :question_id, message: 'は同じ質問に対して一回しか回答できません' }
 end
